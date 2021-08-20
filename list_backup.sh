4d2b1e6fed2d0dfe20012885b2a3263960bb5343
midhlajvs <midhlaj.vs@gmail.com>
2021-08-20 18:29:40 +0530

    Update list_backup.sh

--- list_backup.sh
+++ list_backup.sh
@@ -1,3 +1,11 @@
-BASEDIR=$(dirname "$0")
-. $BASEDIR/ev-backup.config
-duplicity collection-status azure://$AZURE_CONTAINER
+#!/bin/bash 
+
+export SOURCE_FOLDER=/usr/local/src/data
+export DESTINATION_FOLDER=file:///var/rishil/backup
+export PASSPHRASE=PvHfriGsl5l0GpVi
+
+duplicity collection-status ${DESTINATION_FOLDER}
+
+unset SOURCE_FOLDER
+unset DESTINATION_FOLDER
+unset PASSPHRASE
