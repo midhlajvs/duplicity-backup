3b7264678f1a83e7acb537587547e9df5abbc9a7
midhlajvs <midhlaj.vs@gmail.com>
2021-08-20 18:32:02 +0530

    Update backup script

--- backup.sh
+++ backup.sh
@@ -4,9 +4,27 @@ export SOURCE_FOLDER=/usr/local/src/data
 export DESTINATION_FOLDER=file:///var/rishil/backup
 export PASSPHRASE=PvHfriGsl5l0GpVi
 
-echo "Running backup ...."
 
-duplicity --full-if-older-than 24H ${SOURCE_FOLDER} ${DESTINATION_FOLDER}
+function backup {
+
+   echo "Running backup ...."
+   duplicity --full-if-older-than=24h ${SOURCE_FOLDER} ${DESTINATION_FOLDER}
+
+}
+
+function remove {
+
+  echo "Removing backups older than 48 hours ...."
+  duplicity remove-older-than 48h ${DESTINATION_FOLDER}
+
+}
+
+backup
+remove
+
+unset SOURCE_FOLDER
+unset DESTINATION_FOLDER
+unset PASSPHRASE
 
 
 
